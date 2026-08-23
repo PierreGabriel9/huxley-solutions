39430e7e5e3cdfae7b323fcf37e7bad9f880509f
PierreGabriel9 <pierregabrielnm@gmail.com>
2026-08-23 16:09:50 -0300

    simulado

--- duelo-no-ru.c
+++ duelo-no-ru.c
@@ -3,14 +3,160 @@
 #include <math.h>
 #include <stdlib.h>
 
-double bonus_caso13(int classe1, int classe2);
-double bonus_caso23(int classe1, int classe2);
+double bonus1(int classe1, int classe2)
+{
+    if (classe1 == 1 && classe2 == 3)
+    {
+        return 1.3;
+    }
+    return 1;
+}
+
+double bonus2(int classe1, int classe2)
+{
+    if (classe1 == 3 && classe2 == 2)
+    {
+        return 1.25;
+    }
+    return 1;
+}
+
+double bonus3(int classe1, int classe2)
+{
+    if (classe1 == 2 && classe2 == 1)
+    {
+        return 1.15;
+    }
+    return 1;
+}
+
+int duelos(double vida1, double vida2, double ataque1, double ataque2)
+{
+    // se o retorno for 1 é pedro, se for 2 é tulio
+
+    if (vida1 < vida2)
+    {
+        vida2 = vida2 - ataque1;
+        if (vida2 > 0)
+        {
+            vida1 = vida1 - ataque2;
+            if (vida1 <= 0)
+            {
+                return 2;
+            }
+            else if (vida1 > 0 && vida2 > 0)
+            {
+                if (vida1 > vida2)
+                {
+                    return 1;
+                }
+                else if (vida2 > vida1)
+                {
+                    return 2;
+                }
+                else
+                {
+                    if (ataque1 > ataque2)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return 2;
+                    }
+                }
+            }
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else if (vida2 < vida1)
+    {
+        vida1 = vida1 - ataque2;
+        if (vida1 > 0)
+        {
+            vida2 = vida2 - ataque1;
+            if (vida2 <= 0)
+            {
+                return 1;
+            }
+            else if (vida1 > 0 && vida2 > 0)
+            {
+                if (vida1 > vida2)
+                {
+                    return 1;
+                }
+                else if (vida2 > vida1)
+                {
+                    return 2;
+                }
+                else
+                {
+                    if (ataque1 > ataque2)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return 2;
+                    }
+                }
+            }
+        }
+        else
+        {
+            return 2;
+        }
+    }
+    else
+    {
+        vida2 = vida2 - ataque1;
+        if (vida2 > 0)
+        {
+            vida1 = vida1 - ataque2;
+            if (vida1 <= 0)
+            {
+                return 2;
+            }
+            if (vida1 > 0 && vida2 > 0)
+            {
+                if (vida1 > vida2)
+                {
+                    return 1;
+                }
+                else if (vida2 > vida1)
+                {
+                    return 2;
+                }
+                else
+                {
+                    if (ataque1 > ataque2)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return 2;
+                    }
+                }
+            }
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
 
 int main() 
 {
     int c1, c2, c3, c4, c5, c6;
     double atk1, atk2, atk3, atk4, atk5, atk6;
     double vd1, vd2, vd3, vd4, vd5, vd6;
+    int pedro = 0;
+    int tulio = 0;
 
     scanf("%d %lf %lf", &c1, &atk1, &vd1);
     scanf("%d %lf %lf", &c2, &atk2, &vd2);
@@ -19,146 +165,81 @@ int main()
     scanf("%d %lf %lf", &c5, &atk5, &vd5);
     scanf("%d %lf %lf", &c6, &atk6, &vd6);
 
-    vd1 *= bonus_caso13(c1, c4);
-    vd4 *= bonus_caso13(c4, c1);
-    vd2 *= bonus_caso13(c2, c5);
-    vd5 *= bonus_caso13(c5, c2);
-    vd3 *= bonus_caso13(c3, c6);
-    vd6 *= bonus_caso13(c6, c3);
+    // bonus1
 
-    atk1 *= bonus_caso23(c1, c4);
-    atk4 *= bonus_caso23(c4, c1);
-    atk2 *= bonus_caso23(c2, c5);
-    atk5 *= bonus_caso23(c5, c2);
-    atk3 *= bonus_caso23(c3, c6);
-    atk6 *= bonus_caso23(c6, c3);
+    vd1 *= bonus1(c1, c4);
+    vd4 *= bonus1(c4, c1);
+    vd2 *= bonus1(c2, c5);
+    vd5 *= bonus1(c5, c2);
+    vd3 *= bonus1(c3, c6);
+    vd6 *= bonus1(c6, c3);
 
-    /*if (vd4 - atk1 <= 0 && vd1 - atk4 != 0)
+    // bonus2
+
+    atk1 *= bonus2(c1, c4);
+    atk4 *= bonus2(c4, c1);
+    atk2 *= bonus2(c2, c5);
+    atk5 *= bonus2(c5, c2);
+    atk3 *= bonus2(c3, c6);
+    atk6 *= bonus2(c6, c3);
+
+    // bonus3
+
+    vd1 *= bonus3(c1, c4);
+    vd4 *= bonus3(c4, c1);
+    vd2 *= bonus3(c2, c5);
+    vd5 *= bonus3(c5, c2);
+    vd3 *= bonus3(c3, c6);
+    vd6 *= bonus3(c6, c3);
+
+    atk1 *= bonus3(c1, c4);
+    atk4 *= bonus3(c4, c1);
+    atk2 *= bonus3(c2, c5);
+    atk5 *= bonus3(c5, c2);
+    atk3 *= bonus3(c3, c6);
+    atk6 *= bonus3(c6, c3);
+
+    //duelos(int vida1, int vida2, int ataque1, int ataque2)
+
+    if (duelos(vd1, vd4, atk1, atk4) == 1)
     {
+        pedro++;
         printf("Rodada1: Pedro\n");
     }
-    else if (vd1 - atk4 <= 0 && vd4 - atk1 != 0)
+    else
     {
+        tulio++;
         printf("Rodada1: Tulio\n");
     }
-    else if (vd1 - atk4 > 0 && vd4 - atk1 > 0)
-    {
-        if (vd1 > vd4)
-        {
-            printf("Rodada1: Pedro\n");
-        }
-        else if (vd4 > vd1)
-        {
-            printf("Rodada1: Tulio\n");
-        }
-        else
-        {
-            if (atk1 > atk4)
-            {
-                printf("Rodada1: Pedro\n");
-            }
-            else
-            {
-                printf("Rodada1: Tulio\n");
-            }
-        }
-    }
 
-    if (vd5 - atk2 <= 0 && vd2 - atk5 != 0)
+    if (duelos(vd2, vd5, atk2, atk5) == 1)
     {
+        pedro++;
         printf("Rodada2: Pedro\n");
     }
-    else if (vd2 - atk5 <= 0 && vd5 - atk2 != 0)
+    else
     {
+        tulio++;
         printf("Rodada2: Tulio\n");
     }
-    else if (vd2 - atk5 > 0 && vd5 - atk2 > 0)
-    {
-        if (vd2 > vd5)
-        {
-            printf("Rodada2: Pedro\n");
-        }
-        else if (vd5 > vd2)
-        {
-            printf("Rodada2: Tulio\n");
-        }
-        else
-        {
-            if (atk2 > atk5)
-            {
-                printf("Rodada2: Pedro\n");
-            }
-            else
-            {
-                printf("Rodada2: Tulio\n");
-            }
-        }
-    }
 
-    if (vd6 - atk3 <= 0 && vd3 - atk6 != 0)
+    if (duelos(vd3, vd6, atk3, atk6) == 1)
     {
+        pedro++;
         printf("Rodada3: Pedro\n");
     }
-    else if (vd3 - atk6 <= 0 && vd6 - atk3 != 0)
+    else
     {
+        tulio++;
         printf("Rodada3: Tulio\n");
     }
-    else if (vd3 - atk6 > 0 && vd6 - atk3 > 0)
+
+    if (pedro > tulio)
     {
-        if (vd3 > vd6)
-        {
-            printf("Rodada3: Pedro\n");
-        }
-        else if (vd6 > vd3)
-        {
-            printf("Rodada3: Tulio\n");
-        }
-        else
-        {
-            if (atk3 > atk6)
-            {
-                printf("Rodada3: Pedro\n");
-            }
-            else
-            {
-                printf("Rodada3: Tulio\n");
-            }
-        }
-    }*/
-
-
-
-
-
-
-
-
-    
-	return 0;
-}
-
-double bonus_caso13 (int classe1, int classe2)
-{
-    if (classe1 == 1 && classe2 == 3)
-    {
-        return 1.3;
+        printf("Pedro vitorioso\n");
     }
-    else if (classe1 == 2 && classe2 == 1)
+    else
     {
-        return 1.15;
+        printf("Tulio vitorioso\n");
     }
-    return 1;
-}
-
-double bonus_caso23 (int classe1, int classe2)
-{
-    if (classe1 == 3 && classe2 == 2)
-    {
-        return 1.25;
-    }
-    else if (classe1 == 2 && classe2 == 1)
-    {
-        return 1.15;
-    }
-    return 1;
 }
